33bbb186584861b4da3eaaae1c4ac0ae00398496
buhtolin <vkotsev@mustackable.dev>
2026-09-16 14:43:47 +0300

    Adding array support

--- src/Ormamu/Cache/Cache.cs
+++ src/Ormamu/Cache/Cache.cs
@@ -20,40 +20,45 @@ internal static class Cache
         foreach (var type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes()))
         {
             TableAttribute? tableAttribute = type.GetCustomAttribute<TableAttribute>();
-            if(tableAttribute is null) continue;
-            
+            if (tableAttribute is null) continue;
+
             OrmamuConfigIdAttribute? configAttribute = type.GetCustomAttribute<OrmamuConfigIdAttribute>();
             OrmamuOptions options = buildOptions.First().Value;
             if (configAttribute is not null && !buildOptions.TryGetValue(configAttribute.ConfigId, out options!))
             {
-                throw new CacheBuilderException(CacheBuilderExceptionType.ConfigNotFound, configAttribute.ConfigId, type.Name);
+                throw new CacheBuilderException(
+                    CacheBuilderExceptionType.ConfigNotFound,
+                    configAttribute.ConfigId,
+                    type.Name);
             }
-            
+
             List<PropertyMapping> properties = new();
             List<PropertyMapping> keyProperties = new();
             Dictionary<string, string> customPropertyNameDictionary = new();
-            
+
             ExtractProperties(properties, keyProperties, customPropertyNameDictionary, type, options);
-            
+
             CompositeKeyAttribute? compositeKeyAttribute = type.GetCustomAttribute<CompositeKeyAttribute>();
-            
+
             if (compositeKeyAttribute is not null)
             {
                 keyProperties = ConvertToCompositeKeyProperties(compositeKeyAttribute, keyProperties, options, type);
             }
-            
+
             if (keyProperties.Count == 0) continue;
-            
+
             string paddingSymbol = options.Dialect switch
             {
                 SqlDialect.PostgreSql => "\"",
                 SqlDialect.MySql or SqlDialect.MariaDb => "`",
-                _=> ""
+                _ => ""
             };
-            string columnsString = String.Join(",", properties
-                .Where(x => !x.IsDbGenerated)
-                .Select(x => $"{paddingSymbol}{x.DbName}{paddingSymbol}"));
-            
+            string columnsString = String.Join(
+                ",",
+                properties
+                    .Where(x => !x.IsDbGenerated)
+                    .Select(x => $"{paddingSymbol}{x.DbName}{paddingSymbol}"));
+
             cache.TryAdd(
                 type,
                 new(
@@ -64,23 +69,17 @@ internal static class Cache
                     properties.ToArray()
                 )
             );
-            
+
             Func<string, string> typeMapConverter = options.NameConverter;
             if (customPropertyNameDictionary.Count > 0)
             {
-                FrozenDictionary<string, string> lookUpDictionary = customPropertyNameDictionary.ToFrozenDictionary();
                 typeMapConverter = x =>
-                {
-                    if (lookUpDictionary.TryGetValue(x, out string? value))
-                    {
-                        return value;
-                    }
-
-                    return options.NameConverter(x);
-                };
+                    customPropertyNameDictionary.TryGetValue(x, out string? value) ? value : options.NameConverter(x);
             }
+
             SqlMapper.SetTypeMap(type, new ConfigurableTypeMap(type, typeMapConverter));
         }
+
         BuilderDataCache = cache.ToFrozenDictionary();
     }
 
@@ -94,10 +93,10 @@ internal static class Cache
         foreach (PropertyInfo property in type.GetProperties(options.PropertyBindingFlags))
         {
             PropertyData? data = GetPropertyData(property);
-                
+
             if (data is null) continue;
-            if(data.HasCustomName) customPropertyNameDictionary.Add(data.AssemblyName, data.Name);
-                
+            if (data.HasCustomName) customPropertyNameDictionary.Add(data.AssemblyName, data.Name);
+
             PropertyMapping mapping = new(
                 data.HasCustomName ? data.Name : options.NameConverter(data.Name),
                 data.AssemblyName,
@@ -105,7 +104,7 @@ internal static class Cache
                 data.IsDbGenerated,
                 data.Type,
                 GenerateGetter(type, property));
-                
+
             if (data.IsKey) keyProperties.Add(mapping);
             properties.Add(mapping);
         }
@@ -117,7 +116,8 @@ internal static class Cache
         OrmamuOptions options,
         Type type)
     {
-        PropertyInfo[] compositeKeyProperties = compositeKeyAttribute.KeyType.GetProperties(options.PropertyBindingFlags);
+        PropertyInfo[] compositeKeyProperties =
+            compositeKeyAttribute.KeyType.GetProperties(options.PropertyBindingFlags);
         return keyProperties.Select(x =>
         {
             PropertyInfo? property = compositeKeyProperties.FirstOrDefault(y => x.AssemblyName == y.Name);
@@ -139,11 +139,10 @@ internal static class Cache
 
     private static string GetDbObjectIdentifier(this TableAttribute attribute, SqlDialect dialect, string paddingSymbol)
     {
-        
         StringBuilder sb = new(paddingSymbol);
         if (
             attribute.Schema is not null &&
-            !Array.Exists([SqlDialect.MySql, SqlDialect.MariaDb, SqlDialect.Sqlite], x=>x==dialect))
+            !Array.Exists([SqlDialect.MySql, SqlDialect.MariaDb, SqlDialect.Sqlite], x => x == dialect))
         {
             sb
                 .Append(attribute.Schema)
@@ -151,74 +150,87 @@ internal static class Cache
                 .Append(".")
                 .Append(paddingSymbol);
         }
+
         sb.Append(attribute.Name).Append(paddingSymbol);
-        
+
         return sb.ToString();
     }
 
     private static PropertyData? GetPropertyData(PropertyInfo property)
     {
-        if(property.PropertyType.IsValueType || property.PropertyType == typeof(string))
+        if (property.PropertyType.IsArray)
         {
-            bool isKey = false;
-            bool isDbGenerated = false;
-            
-            if(property.GetCustomAttribute<NotMappedAttribute>() is not null) return null;
-            KeyAttribute? keyAttribute = property.GetCustomAttribute<KeyAttribute>();
-            
-            if (keyAttribute is not null)
-            {
-                isKey = true;
-                isDbGenerated = true;
-            }
-            
-            DatabaseGeneratedAttribute? dbGeneratedAttribute = property.GetCustomAttribute<DatabaseGeneratedAttribute>();
-
-            if (dbGeneratedAttribute is not null)
-            {
-                isDbGenerated = dbGeneratedAttribute.DatabaseGeneratedOption != DatabaseGeneratedOption.None;
-            }
-            
-            ColumnAttribute? columnAttribute = property.GetCustomAttribute<ColumnAttribute>();
-            if (columnAttribute is not null && !string.IsNullOrEmpty(columnAttribute.Name))
-            {
-                return new(columnAttribute.Name, property.Name, isKey, isDbGenerated, property.PropertyType, true);
-            }
-            return new(property.Name, property.Name, isKey, isDbGenerated, property.PropertyType);
+            if (!property.PropertyType.GetElementType()!.IsValidType())
+                return null;
+        }
+        else
+        {
+            if (!property.PropertyType.IsValidType())
+                return null;
         }
 
-        return null;
+        bool isKey = false;
+        bool isDbGenerated = false;
+
+        if (property.GetCustomAttribute<NotMappedAttribute>() is not null) return null;
+        KeyAttribute? keyAttribute = property.GetCustomAttribute<KeyAttribute>();
+
+        if (keyAttribute is not null)
+        {
+            isKey = true;
+            isDbGenerated = true;
+        }
+
+        DatabaseGeneratedAttribute? dbGeneratedAttribute = property.GetCustomAttribute<DatabaseGeneratedAttribute>();
+
+        if (dbGeneratedAttribute is not null)
+        {
+            isDbGenerated = dbGeneratedAttribute.DatabaseGeneratedOption != DatabaseGeneratedOption.None;
+        }
+
+        ColumnAttribute? columnAttribute = property.GetCustomAttribute<ColumnAttribute>();
+        if (columnAttribute is not null && !string.IsNullOrEmpty(columnAttribute.Name))
+        {
+            return new(columnAttribute.Name, property.Name, isKey, isDbGenerated, property.PropertyType, true);
+        }
+
+        return new(property.Name, property.Name, isKey, isDbGenerated, property.PropertyType);
     }
+
+    private static bool IsValidType(this Type type)
+        => type.IsValueType || type == typeof(string);
 
     internal static CommandBuilderData ResolveCommandBuilderData(Type identifier)
     {
         if (BuilderDataCache is null)
         {
-            lock(ConfigLock)
+            lock (ConfigLock)
             {
-                if(BuilderDataCache is null)
+                if (BuilderDataCache is null)
                     OrmamuConfig.Apply();
             }
         }
-        
+
         if (BuilderDataCache!.TryGetValue(identifier, out CommandBuilderData? entry))
         {
             return entry;
         }
-        
+
         throw new CommandBuilderException(CommandBuilderExceptionType.MissingClass, identifier.Name);
     }
+
     private static Func<object, object> GenerateGetter(Type baseClass, PropertyInfo property)
     {
         Type objectType = typeof(object);
         ParameterExpression parameterExpression = Expression.Parameter(objectType, "x");
         UnaryExpression memberExpression = Expression.Convert(
-                                                Expression.Property(
-                                                    Expression.Convert(parameterExpression, baseClass),
-                                                    property),
-                                                objectType);
+            Expression.Property(
+                Expression.Convert(parameterExpression, baseClass),
+                property),
+            objectType);
         return Expression.Lambda<Func<object, object>>(memberExpression, parameterExpression).Compile();
     }
+
     private sealed record PropertyData(
         string Name,
         string AssemblyName,
